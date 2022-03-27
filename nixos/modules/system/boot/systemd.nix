8334b8e6a799406e41bab8c729ebd194983eb8f1
Nathan Ringo <nathan@remexre.xyz>
2022-03-26 22:04:25 -0500

    nixos/systemd: make kbrequest target configurable

--- nixos/modules/system/boot/systemd.nix
+++ nixos/modules/system/boot/systemd.nix
@@ -367,6 +367,14 @@ in
       '';
     };
 
+    systemd.kbrequestUnit = mkOption {
+      type = types.str;
+      example = "rescue.target";
+      description = ''
+        Target that should be started when Alt+ArrowUp is pressed on the console.
+      '';
+    };
+
     globalEnvironment = mkOption {
       type =
         with types;
