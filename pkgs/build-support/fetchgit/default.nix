85451f47aebbda8eee0d8561034dcdc34587935d
Finn Landweber <finn@landweber.xyz>
2024-07-27 17:18:45 +0200

    fetchgit: add signature verification
    Add `verifyCommit` and `publicKeys` attributes to check git commit
    signatures.

--- pkgs/build-support/fetchgit/default.nix
+++ pkgs/build-support/fetchgit/default.nix
@@ -1,4 +1,4 @@
-{lib, stdenvNoCC, git, git-lfs, cacert}: let
+{lib, stdenvNoCC, git, git-lfs, cacert, runCommand, writeText, openssh}: let
   urlToName = url: rev: let
     inherit (lib) removeSuffix splitString last;
     base = last (splitString ":" (baseNameOf (removeSuffix "/" url)));
@@ -35,6 +35,8 @@ lib.makeOverridable (lib.fetchers.withNormalizedHash { } (
   netrcImpureEnvVars ? []
 , meta ? {}
 , allowedRequisites ? null
+, publicKeys ? []
+, verifyCommit ? false
 }:
 
 /* NOTE:
@@ -82,10 +84,9 @@ in
 if builtins.isString sparseCheckout then
   # Changed to throw on 2023-06-04
   throw "Please provide directories/patterns for sparse checkout as a list of strings. Passing a (multi-line) string is not supported any more."
-else
-stdenvNoCC.mkDerivation {
+else let
+fetchresult = stdenvNoCC.mkDerivation {
   name = if name != null then name else urlToName url revWithTag;
-
   builder = ./builder.sh;
   fetcher = ./nix-prefetch-git;
 
@@ -100,8 +101,9 @@ stdenvNoCC.mkDerivation {
   # > from standard in as a newline-delimited list instead of from the arguments.
   sparseCheckout = builtins.concatStringsSep "\n" sparseCheckout;
 
-  inherit url leaveDotGit fetchLFS fetchSubmodules deepClone branchName nonConeMode postFetch;
+  inherit url fetchLFS fetchSubmodules deepClone branchName nonConeMode postFetch;
   rev = revWithTag;
+  leaveDotGit = leaveDotGit || verifyCommit;
 
   postHook = if netrcPhase == null then null else ''
     ${netrcPhase}
@@ -121,5 +123,20 @@ stdenvNoCC.mkDerivation {
   passthru = {
     gitRepoUrl = url;
   };
-}
+};
+allowedSignersFile = writeText "allowed signers" (lib.concatMapStrings (k: "* ${k.type} ${k.key}\n") publicKeys);
+in
+if verifyCommit then
+runCommand name {
+  buildInputs = [git openssh];
+  inherit leaveDotGit;
+    } ''
+  git -c gpg.ssh.allowedSignersFile="${allowedSignersFile}" -c safe.directory='*' -C "${fetchresult}" verify-commit ${rev}
+  cp -r --no-preserve=all "${fetchresult}" $out
+  if test "$leaveDotGit" != 1; then
+      rm -rf "$out"/.git
+  fi
+''
+else
+fetchresult
 ))
