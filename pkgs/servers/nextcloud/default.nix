NixOS/nixpkgs

nextcloud: compress static content at build time
The schema is similar to Mastodon's packaging script, gnerating gzip and brotli compressed files ahead-of-time.

The find command only selects those folders that are publicly viewable according to the nginx config, and only compresses file types that are allowed in the nginx config.
The index.html and robots.txt files are too small to be relevant.

The gzip files are used through the default nginx config, brotli requires the administrators to set `services.nginx.recommendedBrotliSettings = true`.

560MB /nix/store/i6b1aqrbjgvx6jc8psv1kcyhkayqg8xx-nextcloud-27.0.0 (previous)
683MB /nix/store/h2ai1zkdqp280xv4wp7xrfn4ax86l9xf-nextcloud-27.0.0 (new)

I observed the login page with Firefox. The server has `services.nginx.recommendedGzipSettings` and `services.nginx.recommendedBrotliSettings` enabled.
_16.53MB uncompressed (according to Firefox dev tools)_
3.94MB on-the-fly compressed (old)
1.64MB ahead-of-time compressed (new)
That's a 2.4x compression improvement!

No measurements for CPU compute savings on the server side, but we avoid compressing the same files over and over again. Helps the environment :earth_africa:

Brotli and gzip should be equally fast at decompression irrespective of compression level.

pkgs/servers/nextcloud/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl, nixosTests
+{ lib, stdenv, zopfli, brotli, fetchurl, nixosTests
 , nextcloud27Packages
 , nextcloud26Packages
 , nextcloud25Packages
@@ -27,6 +27,16 @@ let
       tests = nixosTests.nextcloud;
       inherit packages;
     };
+
+    nativeBuildInputs = [ zopfli brotli ];
+
+    buildPhase = ''
+      # Create missing static gzip and brotli files
+      find apps/ core/ dist/ resources/ themes/ \
+        -type f -regextype posix-extended -iregex '.*\.(css|js|json|svg|ico|txt|md|xml|html|ttf|otf|eot)' \
+        -exec zopfli --gzip {} ';' \
+        -exec brotli --best --keep {} ';'
+    '';
 
     installPhase = ''
       runHook preInstall

amansakhuja/nixpkgs5

pkgs/servers/nextcloud/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenvNoCC, fetchurl, nixosTests
+{ lib, stdenvNoCC, zopfli, brotli, fetchurl, nixosTests
 , nextcloud28Packages
 , nextcloud29Packages
 }:
@@ -21,6 +21,16 @@ let
       tests = nixosTests.nextcloud;
       inherit packages;
     };
+
+    nativeBuildInputs = [ zopfli brotli ];
+
+    buildPhase = ''
+      # Create missing static gzip and brotli files
+      find apps/ core/ dist/ resources/ themes/ \
+        -type f -regextype posix-extended -iregex '.*\.(css|js|json|svg|ico|txt|md|xml|html|ttf|otf|eot)' \
+        -exec zopfli --gzip {} ';' \
+        -exec brotli --best --keep {} ';'
+    '';
 
     installPhase = ''
       runHook preInstall