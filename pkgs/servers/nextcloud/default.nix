a367f14d439222c5fbab48ea41fce2da88a9eeec
Jonas Heinrich <onny@project-insanity.org>
2024-11-06 16:23:49 +0100

    nextcloud*: build from source

--- pkgs/servers/nextcloud/default.nix
+++ pkgs/servers/nextcloud/default.nix
@@ -1,11 +1,13 @@
 {
   lib,
-  stdenvNoCC,
-  fetchurl,
+  stdenv,
+  fetchFromGitHub,
+  fetchNpmDeps,
   nixosTests,
   nextcloud28Packages,
   nextcloud29Packages,
   nextcloud30Packages,
+  nodejs,
 }:
 
 let
@@ -13,17 +15,29 @@ let
     {
       version,
       hash,
+      npmHash,
       eol ? false,
       extraVulnerabilities ? [ ],
       packages,
     }:
-    stdenvNoCC.mkDerivation rec {
+    stdenv.mkDerivation (finalAttrs: {
       pname = "nextcloud";
       inherit version;
 
-      src = fetchurl {
-        url = "https://download.nextcloud.com/server/releases/nextcloud-${version}.tar.bz2";
+      src = fetchFromGitHub {
+        owner = "nextcloud";
+        repo = "server";
+        rev = "refs/tags/v${version}";
         inherit hash;
+        fetchSubmodules = true;
+      };
+
+      nativeBuildInputs = [ nodejs ];
+
+      npmDeps = fetchNpmDeps {
+        inherit (finalAttrs) src;
+        name = "${finalAttrs.pname}-npm-deps";
+        hash = npmHash;
       };
 
       passthru = {
@@ -55,27 +69,27 @@ let
         knownVulnerabilities =
           extraVulnerabilities ++ (lib.optional eol "Nextcloud version ${version} is EOL");
       };
-    };
+    });
 in
 {
   nextcloud28 = generic {
     version = "28.0.11";
-    hash = "sha256-S6rs7GpvFFgy28PGNdcuIM1IBKytmmZOanS5CnmB40g=";
+    hash = "sha256-Af+QBkWGpQfel2se3DFXNYzX9X3T4lix2Cm9kWizzIM=";
+    npmHash = "sha256-1mTc3yIItE4aPStGA6ILnt3YqRYagzc6wHsX/76qpkE=";
     packages = nextcloud28Packages;
   };
 
   nextcloud29 = generic {
     version = "29.0.8";
-    hash = "sha256-CrVLUX92zSbyvTi2/hhLn7rtMvc0JGxYwaz4NHPApLk=";
+    hash = "sha256-I+HdAfLo3NHv4FA6dqr5vecjyiFsccRN2IYC1KHxBDo=";
+    npmHash = "sha256-XD3pj+Ygf8WdNHQM8q84jbKa41LYDOtxK5DaAxIUMiY=";
     packages = nextcloud29Packages;
   };
 
   nextcloud30 = generic {
     version = "30.0.1";
-    hash = "sha256-eewv+tYjG9j8xKuqzBLlrFHmcNCJr/s3lINZLNoP3Ms=";
+    hash = "sha256-cE5YLeDhw1pL++zwX2H+/H6WvwXV+yvaQOS/MQP0GPM=";
+    npmHash = "sha256-DvWidXB7FF+4pbkZnEiP+Dk49wvS2fZEZSh8O5nmoNM=";
     packages = nextcloud30Packages;
   };
-
-  # tip: get the sha with:
-  # curl 'https://download.nextcloud.com/server/releases/nextcloud-${version}.tar.bz2.sha256'
 }
