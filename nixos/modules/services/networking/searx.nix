949053aa5de6180e2d2b70f163f9d097b5a8fde2
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-11-18 13:52:28 +0100

    nixos/searx: don't delete /run/searx/ on init service restarts

--- nixos/modules/services/networking/searx.nix
+++ nixos/modules/services/networking/searx.nix
@@ -310,6 +310,7 @@ in
             User = "searx";
             RuntimeDirectory = "searx";
             RuntimeDirectoryMode = "750";
+            RuntimeDirectoryPreserve = "yes";
           }
           // optionalAttrs (cfg.environmentFile != null) {
             EnvironmentFile = cfg.environmentFile;
