17c20bdfb62584f60db3efd82028885c98744494
William Brockhus <pickeringw@gmail.com>
2025-03-18 17:22:20 +1100

    nixos/nbfc-linux: add service
    Facilitates configuration of the NoteBook FanControl service within configuration.nix.

---
+++ nixos/modules/services/hardware/nbfc-linux.nix
@@ -0,0 +1,61 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+with lib;
+
+let
+  cfg = config.services.nbfc-linux;
+
+  nbfcConfigList = splitString "\n" (
+    removeSuffix "\n" (
+      builtins.readFile (
+        pkgs.runCommand "nbfc-config-list" { } ''
+          ${pkgs.nbfc-linux}/bin/nbfc config --list > $out
+        ''
+      )
+    )
+  );
+in
+{
+  options.services.nbfc-linux = {
+    enable = lib.mkEnableOption "NBFC: NoteBook FanControl service";
+
+    package = lib.mkPackageOption pkgs "nbfc-linux" { default = "nbfc-linux "; };
+
+    configName = mkOption {
+      type = types.nullOr types.str;
+      description = "The NBFC configuration name to use. Obtain the list via `nbfc config --list`";
+      default = null;
+    };
+  };
+
+  config = mkIf cfg.enable {
+
+    assertions = [
+      {
+        assertion = (cfg.configName == null) || (builtins.elem cfg.configName nbfcConfigList);
+        message = "nbfc-linux: invalid configName: ${cfg.configName}";
+      }
+    ];
+
+    environment.systemPackages = [ pkgs.nbfc-linux ];
+
+    environment.etc."nbfc-linux/config.json".source = pkgs.writeTextFile {
+      name = "nbfc-linux-config.json";
+      text = ''{"SelectedConfigId": "${cfg.configName}"}'';
+    };
+
+    systemd.services.nbfc-linux = {
+      enable = true;
+      description = "NoteBook FanControl";
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig.Type = "simple";
+      path = [ pkgs.kmod ];
+      script = "${pkgs.nbfc-linux}/bin/nbfc_service --config-file /etc/nbfc-linux/config.json";
+    };
+  };
+}
