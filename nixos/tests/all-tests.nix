a4d4c9806f103d02f2338c3ca1ad57fb6b0b196c
weriomat <engel@weriomat.com>
2025-04-17 13:59:47 +0200

    nixos/tests/cmk-agent: init tests
    Co-authored-by: Chaostheorie <cobalt@cobalt.rocks>

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -311,6 +311,7 @@ in
   cloud-init = handleTest ./cloud-init.nix { };
   cloud-init-hostname = handleTest ./cloud-init-hostname.nix { };
   cloudlog = handleTest ./cloudlog.nix { };
+  cmk-agent = handleTest ./cmk-agent.nix { };
   cntr = handleTestOn [ "aarch64-linux" "x86_64-linux" ] ./cntr.nix { };
   cockpit = handleTest ./cockpit.nix { };
   cockroachdb = handleTestOn [ "x86_64-linux" ] ./cockroachdb.nix { };
