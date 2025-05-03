3f8b5c630e0e547827fccb0013adef6164614a2f
Martin Joerg <martin.joerg@gmail.com>
2025-05-03 08:30:35 +0000

    python3Packages.ezdxf: cleanup

--- pkgs/development/python-modules/ezdxf/default.nix
+++ pkgs/development/python-modules/ezdxf/default.nix
@@ -1,7 +1,6 @@
 {
   lib,
   buildPythonPackage,
-  pythonOlder,
   fetchFromGitHub,
   pyparsing,
   typing-extensions,
@@ -22,8 +21,6 @@ buildPythonPackage rec {
   pname = "ezdxf";
 
   pyproject = true;
-
-  disabled = pythonOlder "3.5";
 
   src = fetchFromGitHub {
     owner = "mozman";
@@ -68,12 +65,13 @@ buildPythonPackage rec {
     "ezdxf.addons"
   ];
 
-  meta = with lib; {
+  meta = {
     description = "Python package to read and write DXF drawings (interface to the DXF file format)";
     mainProgram = "ezdxf";
-    homepage = "https://github.com/mozman/ezdxf/";
-    license = licenses.mit;
-    maintainers = with maintainers; [ hodapp ];
-    platforms = platforms.unix;
+    homepage = "https://ezdxf.mozman.at/";
+    changelog = "https://github.com/mozman/ezdxf/blob/${src.rev}/notes/pages/CHANGELOG.md";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ hodapp ];
+    platforms = lib.platforms.unix;
   };
 }
