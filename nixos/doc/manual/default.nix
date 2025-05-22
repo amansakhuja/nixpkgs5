dfdb7de3d3357a4346015e1808a88ef8e313489b
Adrien Faure <adrien.faure@protonmail.com>
2025-05-22 11:06:06 +0200

    formatting

--- nixos/doc/manual/default.nix
+++ nixos/doc/manual/default.nix
@@ -113,7 +113,9 @@ let
     substituteInPlace ./configuration/configuration.md \
       --replace-fail \
           '@MODULE_CHAPTERS@' \
-          ${escapeShellArg (concatMapStringsSep "\n" (p: builtins.baseNameOf "${p.value}") config.meta.doc)}
+          ${escapeShellArg (
+            concatMapStringsSep "\n" (p: builtins.baseNameOf "${p.value}") config.meta.doc
+          )}
 
     substituteInPlace ./nixos-options.md \
       --replace-fail \
