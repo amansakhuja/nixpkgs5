24bb29001d03b257e92ce930ecee7bcbe06f83d0
Acid Bong <acidbong@tilde.club>
2025-05-25 10:59:20 +0300

    rofi{,-wayland}-unwrapped: refresh, move all Wayland under a bool flag
    - replace `rev` with `tag` in source fetchers
    - replace `rec {}` with `(finalAttrs: {})`
    - remove `with lib`
    - move all Wayland-specific content to `rofi-unwrapped`, so that its
      Wayland counterpart can be created with a single
      `.override {waylandSupport=true;}`

--- pkgs/by-name/ro/rofi-unwrapped/package.nix
+++ pkgs/by-name/ro/rofi-unwrapped/package.nix
@@ -25,19 +25,31 @@
   glib,
   buildPackages,
   pandoc,
+
+  waylandSupport ? false,
+  wayland,
+  wayland-protocols,
+  wayland-scanner,
 }:
-
-stdenv.mkDerivation rec {
-  pname = "rofi-unwrapped";
-  version = "1.7.9.1";
-
-  src = fetchFromGitHub {
+let
+  version = if !waylandSupport then "1.7.9.1" else "1.7.9+wayland1";
+  repoBase = {
     owner = "davatorium";
     repo = "rofi";
-    rev = version;
+    tag = version;
     fetchSubmodules = true;
     hash = "sha256-HZMVGlK6ig7kWf/exivoiTe9J/SLgjm7VwRm+KgKN44=";
   };
+  repoWayland = repoBase // {
+    owner = "lbonn";
+    hash = "sha256-tLSU0Q221Pg3JYCT+w9ZT4ZbbB5+s8FwsZa/ehfn00s=";
+  };
+in
+stdenv.mkDerivation (finalAttrs: {
+  pname = "rofi${lib.optionalString waylandSupport "-wayland"}-unwrapped";
+  inherit version;
+
+  src = fetchFromGitHub (if !waylandSupport then repoBase else repoWayland);
 
   preConfigure = ''
     patchShebangs "script"
@@ -50,42 +62,56 @@ stdenv.mkDerivation rec {
     pkg-config
     glib
   ];
-  nativeBuildInputs = [
-    meson
-    ninja
-    pkg-config
-    flex
-    bison
-    pandoc
-  ];
-  buildInputs = [
-    libxkbcommon
-    pango
-    cairo
-    git
-    librsvg
-    check
-    libstartup_notification
-    libxcb
-    xcb-imdkit
-    xcb-util-cursor
-    xcbutilkeysyms
-    xcbutil
-    xcbutilwm
-    xcbutilxrm
-    which
-  ];
+  nativeBuildInputs =
+    [
+      meson
+      ninja
+      pkg-config
+      flex
+      bison
+      pandoc
+    ]
+    ++ lib.optionals waylandSupport [
+      wayland-protocols
+      wayland-scanner
+    ];
+  buildInputs =
+    [
+      libxkbcommon
+      pango
+      cairo
+      git
+      librsvg
+      check
+      libstartup_notification
+      libxcb
+      xcb-imdkit
+      xcb-util-cursor
+      xcbutilkeysyms
+      xcbutil
+      xcbutilwm
+      xcbutilxrm
+      which
+    ]
+    ++ lib.optionals waylandSupport [
+      wayland
+      wayland-protocols
+    ];
 
   mesonFlags = [ "-Dimdkit=true" ];
 
   doCheck = false;
 
-  meta = with lib; {
-    description = "Window switcher, run dialog and dmenu replacement";
-    homepage = "https://github.com/davatorium/rofi";
-    license = licenses.mit;
-    maintainers = with maintainers; [ bew ];
-    platforms = with platforms; linux;
+  meta = {
+    description =
+      "Window switcher, run dialog and dmenu replacement"
+      + lib.optionalString waylandSupport " for Wayland";
+    inherit (finalAttrs.src.meta) homepage;
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [
+      bew
+    ];
+    platforms = lib.platforms.linux;
     mainProgram = "rofi";
   };
-}
+})
