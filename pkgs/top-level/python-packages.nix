ae7dc4390912ebaa186690acf3484b3a53fffbd2
GueLaKais <koroyeldiores@gmail.com>
2025-05-26 18:45:38 +0200

    python313Packages.colcon-bash: init at 0.5.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2695,6 +2695,8 @@ self: super: with self; {
 
   colcon-argcomplete = callPackage ../development/python-modules/colcon-argcomplete { };
 
+  colcon-bash = callPackage ../development/python-modules/colcon-bash { };
+
   colcon-cargo = callPackage ../development/python-modules/colcon-cargo { };
 
   colcon-defaults = callPackage ../development/python-modules/colcon-defaults { };
