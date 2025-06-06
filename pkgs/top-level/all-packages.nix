e870d0745cea5b413d632c47beaab006dc28f6ef
aleksana <me@aleksana.moe>
2025-06-06 17:27:53 +0800

    asio: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7797,8 +7797,6 @@ with pkgs;
 
   argparse-manpage = with python3Packages; toPythonApplication argparse-manpage;
 
-  asio = callPackage ../development/libraries/asio { };
-
   aspell = callPackage ../development/libraries/aspell { };
 
   aspellDicts = recurseIntoAttrs (callPackages ../development/libraries/aspell/dictionaries.nix { });
