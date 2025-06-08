cb0f0d93afbd23e74c7dfbbd0a1d368af369429f
GetPsyched <priyanshu@getpsyched.dev>
2025-06-08 22:07:02 +0530

    banana-cursor: 1.0.0 -> 2.0.0

--- pkgs/by-name/ba/banana-cursor/package.nix
+++ pkgs/by-name/ba/banana-cursor/package.nix
@@ -1,19 +1,25 @@
 {
   clickgen,
   fetchFromGitHub,
+  fetchzip,
   lib,
   stdenvNoCC,
 }:
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "banana-cursor";
-  version = "1.0.0";
+  version = "2.0.0";
 
   src = fetchFromGitHub {
     owner = "ful1e5";
     repo = "banana-cursor";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-PI7381xf/GctQTnfcE0W3M3z2kqbX4VexMf17C61hT8=";
+    hash = "sha256-DSgc3y4T/mWO9KC6KBy5W7M8TMM0s4U4pnr/Zy3yTMM=";
+  };
+
+  bitmaps = fetchzip {
+    url = "https://github.com/ful1e5/banana-cursor/releases/download/v${finalAttrs.version}/bitmaps.zip";
+    hash = "sha256-EOs5gzd2K75EKvMPHnxpjDPQpbckb/RkosrZB6JxDlk=";
   };
 
   nativeBuildInputs = [
@@ -23,7 +29,10 @@ stdenvNoCC.mkDerivation (finalAttrs: {
   buildPhase = ''
     runHook preBuild
 
-    ctgen build.toml --platforms x11 --bitmaps-dir bitmaps/Banana --out-dir $out/share/icons
+    for dir in "$bitmaps"/*/; do
+      theme_name=$(basename "$dir")
+      ctgen configs/x.build.toml --platforms x11 --bitmaps-dir "$dir" --theme-name "$theme_name" --out-dir $out/share/icons
+    done
 
     runHook postBuild
   '';
