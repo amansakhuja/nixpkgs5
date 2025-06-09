cd971847af9898ce15a305eea7b26f8c56680317
paumr <paum@bdisk.eu>
2025-06-09 13:11:31 +0200

    init: icann-rdap
    On the 27.01.2025 ICANN replaced it's WHOIS services with RDAP.
    Users are now encouraged to use icann-rdap.
    
    source:
    https://www.icann.org/en/announcements/details/icann-update-launching-rdap-sunsetting-whois-27-01-2025-en

---
+++ pkgs/by-name/ic/icann-rdap/package.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  perl,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "icann-rdap";
+  version = "0.0.22";
+
+  src = fetchFromGitHub {
+    owner = "icann";
+    repo = "icann-rdap";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-J8uvFTjFY7YrohxlD7UwzrQUUoHHEb5mzZoQ64XUQHY=";
+  };
+
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-2R6GrcuksEOk8GiVkFhMljS12V2n0J1rCw9MCOtwMjA=";
+
+  nativeBuildInputs = [ perl ];
+
+  meta = {
+    description = "Official ICANN RDAP tools, containing cli client and server";
+    mainProgram = "rdap";
+    homepage = "https://github.com/icann/icann-rdap";
+    license = with lib.licenses; [
+      mit
+      asl20
+    ];
+    maintainers = with lib.maintainers; [ paumr ];
+  };
+})
