NixOS/nixpkgs

fex: Add support for library forwarding

pkgs/by-name/fe/fex/package.nix
@@ -11,19 +11,98 @@
   xxHash,
   fmt,
   nasm,
+  buildEnv,
+  runCommand,
+  writeText,
+  pkgsCross,
+  libclang,
+  libllvm,
+  alsa-lib,
+  libdrm,
+  libGL,
+  wayland,
+  xorg,
 }:
 
+let
+  pkgsCross32 = pkgsCross.gnu32;
+  pkgsCross64 = pkgsCross.gnu64;
+  devRootFSBase = buildEnv {
+    name = "fex-dev-rootfs";
+    paths = [
+      # later stdenv seems to have shuffled around the headers
+      # pkgsCross64.stdenv.cc.libc_dev
+      # pkgsCross32.stdenv.cc.libc_dev
+      # pkgsCross64.stdenv.cc.cc
+      # pkgsCross32.stdenv.cc.cc
+      pkgsCross64.gcc13Stdenv.cc.libc_dev
+      pkgsCross32.gcc13Stdenv.cc.libc_dev
+      pkgsCross64.gcc13Stdenv.cc.cc
+      pkgsCross32.gcc13Stdenv.cc.cc
+
+      alsa-lib.dev
+      libdrm.dev
+      libGL.dev
+      wayland.dev
+      xorg.libX11.dev
+      xorg.libxcb.dev
+      xorg.libXrandr.dev
+      xorg.libXrender.dev
+      xorg.xorgproto
+    ];
+    ignoreCollisions = true;
+    pathsToLink = [
+      "/include"
+      "/lib"
+    ];
+  };
+
+  devRootFS = runCommand "fex-dev-rootfs2" {} ''
+    mkdir -p $out/usr
+    ln -s ${devRootFSBase}/* $out/
+    ln -s ${devRootFSBase}/include $out/usr/
+  '';
+
+  toolchain32 = writeText "toolchain_nix_x86_32.txt" "
+    set(CMAKE_EXE_LINKER_FLAGS_INIT \"-fuse-ld=lld\")
+    set(CMAKE_MODULE_LINKER_FLAGS_INIT \"-fuse-ld=lld\")
+    set(CMAKE_SHARED_LINKER_FLAGS_INIT \"-fuse-ld=lld\")
+    set(CMAKE_SYSTEM_PROCESSOR i686)
+    set(CMAKE_C_COMPILER clang)
+    set(CMAKE_CXX_COMPILER clang++)
+    set(CLANG_FLAGS \"-nodefaultlibs -nostartfiles -lstdc++ -target i686-linux-gnu -msse2 -mfpmath=sse --sysroot=${devRootFS} -iwithsysroot/usr/include\")
+    set(CMAKE_C_COMPILER ${pkgsCross32.buildPackages.clang}/bin/i686-unknown-linux-gnu-clang)
+    set(CMAKE_CXX_COMPILER ${pkgsCross32.buildPackages.clang}/bin/i686-unknown-linux-gnu-clang++)
+    set(CMAKE_C_FLAGS \"\${CMAKE_C_FLAGS} \${CLANG_FLAGS}\")
+    set(CMAKE_CXX_FLAGS \"\${CMAKE_CXX_FLAGS} \${CLANG_FLAGS}\")
+  ";
+
+  toolchain = writeText "toolchain_nix_x86_64.txt" "
+    set(CMAKE_EXE_LINKER_FLAGS_INIT \"-fuse-ld=lld\")
+    set(CMAKE_MODULE_LINKER_FLAGS_INIT \"-fuse-ld=lld\")
+    set(CMAKE_SHARED_LINKER_FLAGS_INIT \"-fuse-ld=lld\")
+    set(CMAKE_SYSTEM_PROCESSOR x86_64)
+    set(CMAKE_C_COMPILER clang)
+    set(CMAKE_CXX_COMPILER clang++)
+    set(CLANG_FLAGS \"-nodefaultlibs -nostartfiles -lstdc++ -target x86_64-linux-gnu --sysroot=${devRootFS} -iwithsysroot/usr/include\")
+    set(CMAKE_C_COMPILER ${pkgsCross64.buildPackages.clang}/bin/x86_64-unknown-linux-gnu-clang)
+    set(CMAKE_CXX_COMPILER ${pkgsCross64.buildPackages.clang}/bin/x86_64-unknown-linux-gnu-clang++)
+    set(CMAKE_C_FLAGS \"\${CMAKE_C_FLAGS} \${CLANG_FLAGS}\")
+    set(CMAKE_CXX_FLAGS \"\${CMAKE_CXX_FLAGS} \${CLANG_FLAGS}\")
+  ";
+in
 llvmPackages.stdenv.mkDerivation (finalAttrs: {
   pname = "fex";
-  version = "2505";
+  # version = "2505";
+  version = "5811914a784cc6fdfdc90242ab7f8ea6ff39ec38";
 
   src = fetchFromGitHub {
-    owner = "FEX-Emu";
+    owner = "neobrain";
     repo = "FEX";
-    tag = "FEX-${finalAttrs.version}";
+    rev = "${finalAttrs.version}";
+    # tag = "FEX-${finalAttrs.version}";
 
-    hash = "sha256-H/BE+x/lwpED10UrbukWHiKJo0L75/KDV2T5q/J0NiM=";
-
+    hash = "sha256-4JDUdxVyuEkefqRamukKgFe8aX4aJs0fMLYCwh3BQS8=";
     leaveDotGit = true;
     postFetch = ''
       cd $out
@@ -49,6 +128,16 @@ llvmPackages.stdenv.mkDerivation (finalAttrs: {
     '';
   };
 
+  postPatch = ''
+    substituteInPlace ThunkLibs/GuestLibs/CMakeLists.txt --replace "/usr/include/libdrm" "${devRootFS}/include/libdrm"
+    substituteInPlace ThunkLibs/HostLibs/CMakeLists.txt --replace "/usr/include/libdrm" "${devRootFS}/include/libdrm"
+    substituteInPlace ThunkLibs/GuestLibs/CMakeLists.txt --replace "/usr/include/wayland" "${devRootFS}/include/wayland"
+    substituteInPlace ThunkLibs/HostLibs/CMakeLists.txt --replace "/usr/include/wayland" "${devRootFS}/include/wayland"
+
+    # Add include paths for thunkgen invocation
+    substituteInPlace ThunkLibs/HostLibs/CMakeLists.txt --replace "-- " "-- $(cat ${llvmPackages.stdenv.cc}/nix-support/libc-cflags) $(cat ${llvmPackages.stdenv.cc}/nix-support/libcxx-cxxflags) $NIX_CFLAGS_COMPILE"
+  '';
+
   nativeBuildInputs = [
     cmake
     ninja
@@ -70,6 +159,21 @@ llvmPackages.stdenv.mkDerivation (finalAttrs: {
     [
       xxHash
       fmt
+      pkgsCross64.buildPackages.clang
+      pkgsCross32.buildPackages.clang
+      libclang
+      libllvm
+
+      # Headers required to build the ThunkLibs subtree
+      alsa-lib.dev
+      libdrm.dev
+      libGL.dev
+      wayland.dev
+      xorg.libX11.dev
+      xorg.libxcb.dev
+      xorg.libXrandr.dev
+      xorg.libXrender.dev
+      xorg.xorgproto
     ]
     ++ (with qt5; [
       qtbase
@@ -85,6 +189,11 @@ llvmPackages.stdenv.mkDerivation (finalAttrs: {
     (lib.cmakeBool "ENABLE_ASSERTIONS" false)
     (lib.cmakeFeature "OVERRIDE_VERSION" finalAttrs.version)
     (lib.cmakeBool "BUILD_TESTS" finalAttrs.finalPackage.doCheck)
+    (lib.cmakeBool "BUILD_THUNKS" true)
+    (lib.cmakeFeature "X86_32_TOOLCHAIN_FILE" "${toolchain32}")
+    (lib.cmakeFeature "X86_64_TOOLCHAIN_FILE" "${toolchain}")
+    (lib.cmakeFeature "X86_DEV_ROOTFS" "${devRootFS}")
+    (lib.cmakeFeature "OVERRIDE_VERSION" "2506")
   ];
 
   strictDeps = true;
@@ -100,6 +209,13 @@ llvmPackages.stdenv.mkDerivation (finalAttrs: {
     "fexcore_apitests"
     "emitter_tests"
   ];
+
+  # placeholder build phase for quick testing
+  # buildPhase = ''
+  #   make drm-host-64 -j5
+  #   make asound-host-64 -j5
+  #   make wayland-client-host-64 -j5
+  # '';
 
   # Avoid wrapping anything other than FEXConfig, since the wrapped executables
   # don't seem to work when registered as binfmts.

amansakhuja/nixpkgs5

pkgs/by-name/fe/fex/package.nix
@@ -11,8 +11,79 @@
   xxHash,
   fmt,
   nasm,
+  buildEnv,
+  writeText,
+  pkgsCross,
+  libclang,
+  libllvm,
+  alsa-lib,
+  libdrm,
+  libGL,
+  wayland,
+  xorg,
 }:
 
+let
+  pkgsCross32 = pkgsCross.gnu32;
+  pkgsCross64 = pkgsCross.gnu64;
+  devRootFS = buildEnv {
+    name = "fex-dev-rootfs";
+    paths = [
+      pkgsCross64.stdenv.cc.libc_dev
+      pkgsCross32.stdenv.cc.libc_dev
+      pkgsCross64.stdenv.cc.cc
+      pkgsCross32.stdenv.cc.cc
+
+      alsa-lib.dev
+      libdrm.dev
+      libGL.dev
+      wayland.dev
+      xorg.libX11.dev
+      xorg.libxcb.dev
+      xorg.libXrandr.dev
+      xorg.libXrender.dev
+      xorg.xorgproto
+    ];
+    ignoreCollisions = true;
+    pathsToLink = [
+      "/include"
+      "/lib"
+    ];
+
+    postBuild = ''
+      mkdir -p $out/usr
+      ln -s $out/include $out/usr/
+    '';
+  };
+
+  toolchain32 = writeText "toolchain_nix_x86_32.txt" ''
+    set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld")
+    set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=lld")
+    set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld")
+    set(CMAKE_SYSTEM_PROCESSOR i686)
+    set(CMAKE_C_COMPILER clang)
+    set(CMAKE_CXX_COMPILER clang++)
+    set(CMAKE_C_COMPILER ${pkgsCross32.buildPackages.clang}/bin/i686-unknown-linux-gnu-clang)
+    set(CMAKE_CXX_COMPILER ${pkgsCross32.buildPackages.clang}/bin/i686-unknown-linux-gnu-clang++)
+    set(CLANG_FLAGS "-nodefaultlibs -nostartfiles -target i686-linux-gnu -msse2 -mfpmath=sse --sysroot=${devRootFS} -iwithsysroot/usr/include")
+    set(CMAKE_C_FLAGS "''${CMAKE_C_FLAGS} ''${CLANG_FLAGS}")
+    set(CMAKE_CXX_FLAGS "''${CMAKE_CXX_FLAGS} ''${CLANG_FLAGS}")
+  '';
+
+  toolchain = writeText "toolchain_nix_x86_64.txt" ''
+    set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld")
+    set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=lld")
+    set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld")
+    set(CMAKE_SYSTEM_PROCESSOR x86_64)
+    set(CMAKE_C_COMPILER clang)
+    set(CMAKE_CXX_COMPILER clang++)
+    set(CMAKE_C_COMPILER ${pkgsCross64.buildPackages.clang}/bin/x86_64-unknown-linux-gnu-clang)
+    set(CMAKE_CXX_COMPILER ${pkgsCross64.buildPackages.clang}/bin/x86_64-unknown-linux-gnu-clang++)
+    set(CLANG_FLAGS "-nodefaultlibs -nostartfiles -target x86_64-linux-gnu --sysroot=${devRootFS} -iwithsysroot/usr/include")
+    set(CMAKE_C_FLAGS "''${CMAKE_C_FLAGS} ''${CLANG_FLAGS}")
+    set(CMAKE_CXX_FLAGS "''${CMAKE_CXX_FLAGS} ''${CLANG_FLAGS}")
+  '';
+in
 llvmPackages.stdenv.mkDerivation (finalAttrs: {
   pname = "fex";
   version = "2506";
@@ -49,6 +120,38 @@ llvmPackages.stdenv.mkDerivation (finalAttrs: {
     '';
   };
 
+  postPatch = ''
+    substituteInPlace ThunkLibs/GuestLibs/CMakeLists.txt ThunkLibs/HostLibs/CMakeLists.txt \
+      --replace-fail "/usr/include/libdrm" "${devRootFS}/include/libdrm" \
+      --replace-fail "/usr/include/wayland" "${devRootFS}/include/wayland"
+
+    # Add include paths for thunkgen invocation
+    substituteInPlace ThunkLibs/HostLibs/CMakeLists.txt \
+      --replace-fail "-- " "-- $(cat ${llvmPackages.stdenv.cc}/nix-support/libc-cflags) $(cat ${llvmPackages.stdenv.cc}/nix-support/libcxx-cxxflags) $NIX_CFLAGS_COMPILE"
+    substituteInPlace ThunkLibs/GuestLibs/CMakeLists.txt \
+      --replace-fail "-- " "-- $(cat ${llvmPackages.stdenv.cc}/nix-support/libcxx-cxxflags)"
+
+    # Patch any references to library wrapper paths
+    substituteInPlace FEXCore/Source/Interface/Config/Config.json.in \
+      --replace-fail "ThunkGuestLibs" "UnusedThunkGuestLibs" \
+      --replace-fail "ThunkHostLibs" "UnusedThunkHostLibs"
+    substituteInPlace FEXCore/Source/Interface/Config/Config.cpp \
+      --replace-fail "FEXCore::Config::CONFIG_THUNKGUESTLIBS" "FEXCore::Config::CONFIG_UNUSEDTHUNKGUESTLIBS" \
+      --replace-fail "FEXCore::Config::CONFIG_THUNKHOSTLIBS" "FEXCore::Config::CONFIG_UNUSEDTHUNKHOSTLIBS"
+    substituteInPlace Source/Tools/LinuxEmulation/VDSO_Emulation.cpp \
+      --replace-fail "FEX_CONFIG_OPT(ThunkGuestLibs, THUNKGUESTLIBS);" "auto ThunkGuestLibs = []() { return \"$out/share/fex-emu/GuestThunks/\"; };" \
+      --replace-fail "FEX_CONFIG_OPT(ThunkGuestLibs32, THUNKGUESTLIBS32);" "auto ThunkGuestLibs32 = []() { return \"$out/share/fex-emu/GuestThunks_32/\"; };"
+    substituteInPlace Source/Tools/LinuxEmulation/LinuxSyscalls/FileManagement.h \
+      --replace-fail "FEX_CONFIG_OPT(ThunkGuestLibs, THUNKGUESTLIBS);" "fextl::string ThunkGuestLibs() { return \"$out/share/fex-emu/GuestThunks/\"; }" \
+      --replace-fail "FEX_CONFIG_OPT(ThunkGuestLibs32, THUNKGUESTLIBS32);" "fextl::string ThunkGuestLibs32() { return \"$out/share/fex-emu/GuestThunks_32/\"; }"
+    substituteInPlace Source/Tools/LinuxEmulation/Thunks.cpp \
+      --replace-fail "FEX_CONFIG_OPT(ThunkHostLibsPath, THUNKHOSTLIBS);" "fextl::string ThunkHostLibsPath() { return \"$out/lib/fex-emu/HostThunks/\"; }" \
+      --replace-fail "FEX_CONFIG_OPT(ThunkHostLibsPath32, THUNKHOSTLIBS32);" "fextl::string ThunkHostLibsPath32() { return \"$out/lib/fex-emu/HostThunks/\"; }"
+    substituteInPlace Source/Tools/FEXConfig/main.qml \
+      --replace-fail "config: \"Thunk" "config: \"UnusedThunk" \
+      --replace-fail "title: qsTr(\"Library forwarding:\")" "visible: false; title: qsTr(\"Library forwarding:\")"
+  '';
+
   nativeBuildInputs = [
     cmake
     ninja
@@ -70,6 +173,21 @@ llvmPackages.stdenv.mkDerivation (finalAttrs: {
     [
       xxHash
       fmt
+      pkgsCross64.buildPackages.clang
+      pkgsCross32.buildPackages.clang
+      libclang
+      libllvm
+
+      # Headers required to build the ThunkLibs subtree
+      alsa-lib.dev
+      libdrm.dev
+      libGL.dev
+      wayland.dev
+      xorg.libX11.dev
+      xorg.libxcb.dev
+      xorg.libXrandr.dev
+      xorg.libXrender.dev
+      xorg.xorgproto
     ]
     ++ (with qt5; [
       qtbase
@@ -85,6 +203,10 @@ llvmPackages.stdenv.mkDerivation (finalAttrs: {
     (lib.cmakeBool "ENABLE_ASSERTIONS" false)
     (lib.cmakeFeature "OVERRIDE_VERSION" finalAttrs.version)
     (lib.cmakeBool "BUILD_TESTS" finalAttrs.finalPackage.doCheck)
+    (lib.cmakeBool "BUILD_THUNKS" true)
+    (lib.cmakeFeature "X86_32_TOOLCHAIN_FILE" "${toolchain32}")
+    (lib.cmakeFeature "X86_64_TOOLCHAIN_FILE" "${toolchain}")
+    (lib.cmakeFeature "X86_DEV_ROOTFS" "${devRootFS}")
   ];
 
   strictDeps = true;