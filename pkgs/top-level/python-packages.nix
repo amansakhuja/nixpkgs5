c8a6cf9123676e779afba80e5ed06f1771b28d61
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2025-06-18 11:59:57 +0200

    python3Packages.gramps-ql: init at 0.4.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6108,6 +6108,8 @@ self: super: with self; {
 
   gramps = callPackage ../development/python-modules/gramps { };
 
+  gramps-ql = callPackage ../development/python-modules/gramps-ql { };
+
   grandalf = callPackage ../development/python-modules/grandalf { };
 
   granian = callPackage ../development/python-modules/granian { };
