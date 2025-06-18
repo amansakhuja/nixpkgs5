e8acecb56b410045e8cbe855ad9c5f758f5be40b
Fang Deng <fang@elfang.com>
2025-06-18 13:22:20 -0400

    rime-wanxiang: add toplevel entries

---
+++ pkgs/by-name/ri/rime-wanxiang/default.nix
@@ -0,0 +1,4 @@
+pkgs: {
+  rime-wanxiang = pkgs.callPackage ./rime-wanxiang.nix {};
+  rime-wanxiang-pro = pkgs.callPackage ./rime-wanxiang-pro.nix {};
+}
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16832,4 +16832,8 @@ with pkgs;
   davis = callPackage ../by-name/da/davis/package.nix {
     php = php83; # https://github.com/tchapi/davis/issues/195
   };
+
+  inherit (import ../by-name/ri/rime-wanxiang pkgs)
+    rime-wanxiang
+    rime-wanxiang-pro;
 }
