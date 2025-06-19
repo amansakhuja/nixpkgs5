NixOS/nixpkgs

photoprism: 250321-57590c48b -> 250426-27ec7a128
https://github.com/photoprism/photoprism/releases/tag/250425-21ddba459
https://github.com/photoprism/photoprism/releases/tag/250426-27ec7a128

pkgs/by-name/ph/photoprism/backend.nix
@@ -2,14 +2,24 @@
   lib,
   buildGoModule,
   coreutils,
-  libtensorflow,
+  python3,
   src,
   version,
   pkg-config,
   vips,
+  runCommand,
 }:
 
-buildGoModule rec {
+let
+  # we need to copy these, to add the symlinks, so the linker actually finds these libraries
+  libtensorflow = runCommand "libtensorflow" { } ''
+    mkdir -p "$out/"
+    cp -R "${python3.pkgs.tensorflow-bin}/${python3.sitePackages}/tensorflow/." "$out/"
+    ln -s "$out/libtensorflow_cc.so.2" "$out/libtensorflow.so"
+    ln -s "$out/libtensorflow_framework.so.2" "$out/libtensorflow_framework.so"
+  '';
+in
+buildGoModule {
   inherit src version;
   pname = "photoprism-backend";
 
@@ -30,18 +40,17 @@ buildGoModule rec {
   ];
 
   postPatch = ''
-    substituteInPlace internal/commands/passwd.go --replace '/bin/stty' "${coreutils}/bin/stty"
+    substituteInPlace internal/commands/passwd.go --replace-warn '/bin/stty' "${coreutils}/bin/stty"
   '';
 
-  vendorHash = "sha256-eHdnTpcVBSvGR9ZiK6A32jfjik8VClDTkv92bD8EIgA=";
+  vendorHash = "sha256-8uy0uLhGOyedqi3AvMsEdDQnFvGgeeZcL4tFgI6bzU8=";
 
   subPackages = [ "cmd/photoprism" ];
 
   # https://github.com/mattn/go-sqlite3/issues/822
-  CGO_CFLAGS = "-Wno-return-local-addr";
+  CGO_CFLAGS = "-Wno-return-local-addr -I${libtensorflow}/include";
 
-  # https://github.com/tensorflow/tensorflow/issues/43847
-  CGO_LDFLAGS = "-fuse-ld=gold";
+  CGO_LDFLAGS = "-L${libtensorflow} -ltensorflow_framework";
 
   meta = with lib; {
     homepage = "https://photoprism.app";

amansakhuja/nixpkgs5

pkgs/by-name/ph/photoprism/backend.nix
@@ -2,14 +2,24 @@
   lib,
   buildGoModule,
   coreutils,
-  libtensorflow,
+  python3,
   src,
   version,
   pkg-config,
   vips,
+  runCommand,
 }:
 
-buildGoModule rec {
+let
+  # we need to copy these, to add the symlinks, so the linker actually finds these libraries
+  libtensorflow = runCommand "libtensorflow" { } ''
+    mkdir -p "$out/"
+    cp -R "${python3.pkgs.tensorflow-bin}/${python3.sitePackages}/tensorflow/." "$out/"
+    ln -s "$out/libtensorflow_cc.so.2" "$out/libtensorflow.so"
+    ln -s "$out/libtensorflow_framework.so.2" "$out/libtensorflow_framework.so"
+  '';
+in
+buildGoModule {
   inherit src version;
   pname = "photoprism-backend";
 
@@ -30,18 +40,17 @@ buildGoModule rec {
   ];
 
   postPatch = ''
-    substituteInPlace internal/commands/passwd.go --replace '/bin/stty' "${coreutils}/bin/stty"
+    substituteInPlace internal/commands/passwd.go --replace-fail '/bin/stty' "${coreutils}/bin/stty"
   '';
 
-  vendorHash = "sha256-eHdnTpcVBSvGR9ZiK6A32jfjik8VClDTkv92bD8EIgA=";
+  vendorHash = "sha256-8uy0uLhGOyedqi3AvMsEdDQnFvGgeeZcL4tFgI6bzU8=";
 
   subPackages = [ "cmd/photoprism" ];
 
   # https://github.com/mattn/go-sqlite3/issues/822
-  CGO_CFLAGS = "-Wno-return-local-addr";
+  CGO_CFLAGS = "-Wno-return-local-addr -I${libtensorflow}/include";
 
-  # https://github.com/tensorflow/tensorflow/issues/43847
-  CGO_LDFLAGS = "-fuse-ld=gold";
+  CGO_LDFLAGS = "-L${libtensorflow} -ltensorflow_framework";
 
   meta = with lib; {
     homepage = "https://photoprism.app";