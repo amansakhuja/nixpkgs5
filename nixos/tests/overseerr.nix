NixOS/nixpkgs

nixos/overseerr: init
Co-authored-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

nixos/tests/overseerr.nix
@@ -1,0 +1,19 @@
+{ lib, ... }:
+{
+  name = "dufs";
+  meta.maintainers = with lib.maintainers; [ jf-uu ];
+
+  nodes.machine =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = [ pkgs.jq ];
+      services.overseerr.enable = true;
+    };
+
+  testScript = ''
+    machine.wait_for_unit("overseerr.service")
+    machine.wait_for_open_port(5055)
+
+    machine.succeed("curl --fail http://localhost:5055/api/v1/status | jq")
+  '';
+}

amansakhuja/nixpkgs5

nixos/tests/overseerr.nix
@@ -1,0 +1,19 @@
+{ lib, ... }:
+{
+  name = "overseerr";
+  meta.maintainers = with lib.maintainers; [ jf-uu ];
+
+  nodes.machine =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = [ pkgs.jq ];
+      services.overseerr.enable = true;
+    };
+
+  testScript = ''
+    machine.wait_for_unit("overseerr.service")
+    machine.wait_for_open_port(5055)
+
+    machine.succeed("curl --fail http://localhost:5055/api/v1/status | jq")
+  '';
+}