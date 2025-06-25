ec6d5e0c358315ae63a126bd24ff0248708face5
Johan Herland <johan@herland.net>
2025-06-25 10:39:38 +0000

    python3Packages.comet-ml: init at 3.49.11

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2777,6 +2777,8 @@ self: super: with self; {
 
   colout = callPackage ../development/python-modules/colout { };
 
+  comet-ml = callPackage ../development/python-modules/comet-ml { };
+
   cometblue-lite = callPackage ../development/python-modules/cometblue-lite { };
 
   comicapi = callPackage ../development/python-modules/comicapi { };
