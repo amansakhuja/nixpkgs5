eca649f5c5786753181dfc3914c00651adb6aa7d
goose121 <goose121@users.noreply.github.com>
2025-06-28 13:23:25 -0600

    exfetch: apply suggested changes
    Co-authored-by: Acid Bong <acidbong@tilde.club>

--- pkgs/by-name/ex/exfetch/package.nix
+++ pkgs/by-name/ex/exfetch/package.nix
@@ -1,29 +1,28 @@
 {
   crystal,
-  fetchgit,
+  fetchFromGitea,
   lib,
-  ...
-}@args:
+}:
 
 crystal.buildCrystalPackage rec {
   pname = "exfetch";
   version = "1.3.5";
 
-  src = fetchgit {
-    url = "https://codeberg.org/Izder456/exfetch.git";
-    rev = "refs/tags/1.3.5";
-    hash = "sha256-Dw6NQBcPNpmLWClJ3uPBPPOF5WBMULAn2vEIl4ewkec===";
+  src = fetchFromGitea {
+    domain = "codeberg.org";
+    owner = "Izder456";
+    repo = "exfetch";
+    tag = version;
+    hash = "sha256-Dw6NQBcPNpmLWClJ3uPBPPOF5WBMULAn2vEIl4ewkec=";
     fetchSubmodules = true;
   };
 
-  buildPhase = ''
-    make bin/exfetch
-  '';
+  buildPhase = "make";
 
-  checkPhase = "";
+  doCheck = false;
 
   meta = {
-    description = "a shell-extensible fetching utility aiming to be a spiritual successor to crfetch, written in crystal";
+    description = "Shell-extensible fetching utility aiming to be a spiritual successor to crfetch, written in crystal";
     mainProgram = "exfetch";
     homepage = "https://codeberg.org/Izder456/exfetch";
     license = lib.licenses.isc;
