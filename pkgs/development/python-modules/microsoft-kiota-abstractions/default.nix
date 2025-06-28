ebed636b8362d948dbacccffc9879f89a4dcc2f1
R. Ryantm <ryantm-bot@ryantm.com>
2025-06-28 19:15:00 +0000

    python3Packages.microsoft-kiota-abstractions: 1.9.3 -> 1.9.4

--- pkgs/development/python-modules/microsoft-kiota-abstractions/default.nix
+++ pkgs/development/python-modules/microsoft-kiota-abstractions/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "microsoft-kiota-abstractions";
-  version = "1.9.3";
+  version = "1.9.4";
   pyproject = true;
 
   disabled = pythonOlder "3.9";
@@ -22,8 +22,8 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "microsoft";
     repo = "kiota-python";
-    tag = "microsoft-kiota-abstractions-v${version}";
-    hash = "sha256-FUfVkJbpD0X7U7DPzyoh+84Bk7C07iLT9dmbUeliFu8=";
+    tag = "microsoft-kiota-serialization-text-v${version}";
+    hash = "sha256-59vuJc7Wb/6PsPA4taAFA2UK8bdz+raZ+NB4S8LahtM=";
   };
 
   sourceRoot = "${src.name}/packages/abstractions/";
