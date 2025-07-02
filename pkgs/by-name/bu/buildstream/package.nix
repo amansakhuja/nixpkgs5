4d7fa4d5f76f7ab3baaf8039b3a93c31f9ca67d6
Dom Rodriguez <shymega@shymega.org.uk>
2025-07-02 22:36:25 +0100

    buildstream: 2.4.1 -> 2.5.0
    Unfortunately, due to a newer set of 'protobuf' and 'grpcio' in Nixpkgs,
    there are several warnings when running `bst`, but these are harmless.
    
    Once 2.5.1 is released, the patch can be removed from this derivation,
    and we can use newer versions of those dependencies.
    
    For now, it is important to get 2.5.0 landed, as it includes a new API
    which is necessary for new releases of Buildstream plugins.

--- pkgs/by-name/bu/buildstream/package.nix
+++ pkgs/by-name/bu/buildstream/package.nix
@@ -20,14 +20,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "buildstream";
-  version = "2.4.1";
+  version = "2.5.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "apache";
     repo = "buildstream";
     tag = version;
-    hash = "sha256-6a0VzYO5yj7EHvAb0xa4xZ0dgBKjFcwKv2F4o93oahY=";
+    hash = "sha256-/kGmAHx10//iVeqLXwcIWNI9FGIi0LlNJW+s6v0yU3Q=";
   };
 
   build-system = with python3Packages; [
@@ -64,6 +64,10 @@ python3Packages.buildPythonApplication rec {
     fuse3
     lzip
     patch
+  ];
+
+  patches = [
+    ./0001-Patch-requirements-for-Protobuf-to-work-with-6.0.patch
   ];
 
   pythonImportsCheck = [ "buildstream" ];
